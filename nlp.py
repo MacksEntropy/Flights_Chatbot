f132bd48909ad6758e429a96bf87443a707490fd
MacksEntropy <andrescovarrubias27@gmail.com>
2024-02-13 16:46:30 -0600

    set up initial chatbot framework

--- nlp.py
+++ nlp.py
@@ -3,13 +3,25 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 def extract_destination(text):
-
     doc = nlp(text)
-    # for ent in doc.ents:
-    #     print(ent)
+    # TODO Figure out case where user specifies destination and location of departure. 
     return [ent for ent in doc.ents if ent.label_ == "GPE"]
+
+def confirm_desination(dest):
+    
+    while not dest:
+        print("Sorry, where would you like to go?")
+        user_input = input()
+        dest = extract_destination(user_input)
+    print("Looking for flights to", dest[0])
+
+def chatbot():
+
+    print('Hello, how can I help you today?')
+    user_input = input()
+    destination = extract_destination(user_input)
+    confirm_desination(destination)
 
 if __name__ == "__main__":
 
-    user_input = "I want to fight into Houston to turn off Google"
-    print(extract_destination(user_input))
+    chatbot()
