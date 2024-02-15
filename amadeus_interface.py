59bf34f1f345a029cbf6076eb67ed07c3a1ebb1d
MacksEntropy <andrescovarrubias27@gmail.com>
2024-02-15 16:36:53 -0600

    converted modules into classes

--- amadeus_interface.py
+++ amadeus_interface.py
@@ -7,62 +7,82 @@ from currency_converter import CurrencyConverter
 
 load_dotenv()
 
-amadeus = Client(
-    client_id=os.getenv('AMADEUS_API_KEY'),
-    client_secret=os.getenv("AMADEUS_PRIV_KEY")
-)
+class Amadeus():
 
-def find_flights(origin : str, dest : str, departure_date: str, num_adults: int) -> list[str]:
-    """
-    :param origin: IATA Code for origin Airport. EX: 'IAH'
-    :param dest:  IATA Code for desination Airport. EX: 'LGA'
-    :param departure_date: Desired date of flight in YYYY-MM-DD format. EX:'2024-02-15'
-    :param num_adults: The number of adult tickets
+    def __init__(self) -> None:
+        self.amadeus = Client(
+            client_id=os.getenv('AMADEUS_API_KEY'),
+            client_secret=os.getenv("AMADEUS_PRIV_KEY")
+        )
 
-    :returns List of top 3 cheapest flights 
-    """
-    try:
-        response = amadeus.shopping.flight_offers_search.get(
-            originLocationCode=origin,
-            destinationLocationCode=dest,
-            departureDate=departure_date,
-            adults=num_adults)
-        # Sort flight offers by grandTotal price (Price including fees)
-        return sorted(response.data, key=lambda x: x['price']['grandTotal'])[:3]
-    except ResponseError as error:
-        print(error)
+    def get_IATA_code(self, city : str) -> list[str]:
+        """
+        Gets the IATA codes for the top 2 airports in a given city
+        """
+        try:
+            response = self.amadeus.reference_data.locations.get(
+            keyword=city,
+            subType=["AIRPORT"])
+            IATA_codes = []
+            for airport in response.data:
+                IATA_codes.append(airport['iataCode'])
+            return IATA_codes[0:2]
+        except ResponseError as error:
+            print(error)
 
-def convert_departure_arrival(iso_datetime):
+    def find_flights(self, origin : str, dest : str, departure_date: str, num_adults: int) -> list[str]:
+        """
+        :param origin: IATA Code for origin Airport. EX: 'IAH'
+        :param dest:  IATA Code for desination Airport. EX: 'LGA'
+        :param departure_date: Desired date of flight in YYYY-MM-DD format. EX:'2024-02-15'
+        :param num_adults: The number of adult tickets
 
-    parsed_datetime = datetime.fromisoformat(iso_datetime)
-    month = parsed_datetime.strftime("%B")  # Full month name
-    day = parsed_datetime.day
-    year = parsed_datetime.year
-    hour_minute = parsed_datetime.strftime("%I:%M %p")  # Hour:Minute AM/PM format
-    formatted_datetime = f"{month} {day}, {year} {hour_minute}"
-    return formatted_datetime
+        :returns List of top 3 cheapest flights 
+        """
+        try:
+            response = self.amadeus.shopping.flight_offers_search.get(
+                originLocationCode=origin,
+                destinationLocationCode=dest,
+                departureDate=departure_date,
+                adults=num_adults)
+            # Sort flight offers by grandTotal price (Price including fees)
+            flight_list = sorted(response.data, key=lambda x: x['price']['grandTotal'])[:3]
+            return self.format_flight_list(flight_list)
+        except ResponseError as error:
+            print(error)
 
-def convert_last_day_to_book(date_str):
+    def convert_departure_arrival(self, iso_datetime):
 
-    parsed_date = datetime.strptime(date_str, "%Y-%m-%d")
-    formatted_date = parsed_date.strftime("%B %d, %Y")
-    return formatted_date
+        parsed_datetime = datetime.fromisoformat(iso_datetime)
+        month = parsed_datetime.strftime("%B")  # Full month name
+        day = parsed_datetime.day
+        year = parsed_datetime.year
+        hour_minute = parsed_datetime.strftime("%I:%M %p")  # Hour:Minute AM/PM format
+        formatted_datetime = f"{month} {day}, {year} {hour_minute}"
+        return formatted_datetime
 
-def format_response(flight_list):
+    def convert_last_day_to_book(self, date_str):
 
-    c = CurrencyConverter()
-    formatted_response = []
-    for flight in flight_list:
-            formatted_response.append(
-            {
-                "departure" : convert_departure_arrival(flight['itineraries'][0]['segments'][0]['departure']['at']),
-                "arrival" : convert_departure_arrival(flight['itineraries'][0]['segments'][0]['arrival']['at']),
-                "price" : f"${round(c.convert(flight['price']['grandTotal'], 'EUR', 'USD'), 2)} USD",
-                "lastDayToBook" : convert_last_day_to_book(flight['lastTicketingDate']),
-                "numSeatsRemaining" : flight['numberOfBookableSeats'],
-            })    
-    return formatted_response
+        parsed_date = datetime.strptime(date_str, "%Y-%m-%d")
+        formatted_date = parsed_date.strftime("%B %d, %Y")
+        return formatted_date
+
+    def format_flight_list(self, flight_list):
+
+        c = CurrencyConverter()
+        formatted_response = []
+        for flight in flight_list:
+                formatted_response.append(
+                {
+                    "departure" : self.convert_departure_arrival(flight['itineraries'][0]['segments'][0]['departure']['at']),
+                    "arrival" : self.convert_departure_arrival(flight['itineraries'][0]['segments'][0]['arrival']['at']),
+                    "price" : f"${round(c.convert(flight['price']['grandTotal'], 'EUR', 'USD'), 2)} USD",
+                    "numSeatsRemaining" : flight['numberOfBookableSeats'],
+                })    
+        return formatted_response
 
 if __name__ == "__main__":
-    flight_list = find_flights('IAH', 'LGA', '2024-02-15', 1)
-    print(format_response(flight_list))
+
+    flightAPI = Amadeus()
+    print(flightAPI.find_flights('IAH', 'LGA', '2024-10-15', 1))
+    # print(flightAPI.get_IATA_code("New York"))
--- nlp.py
+++ nlp.py
@@ -1,79 +1,99 @@
 import spacy
 from spacy.matcher import Matcher
+from datetime import datetime
 
-nlp = spacy.load("en_core_web_sm")
-matcher = Matcher(nlp.vocab)
-origin_pattern = [{"LOWER": {"IN": ["from", "leave"]}}, {"POS": "PROPN", "OP": "+", "ENT_TYPE": "GPE"}]
-destination_pattern = [{"LOWER": {"IN": ["to", "tour", "visit"]}}, {"POS": "PROPN", "OP": "+", "ENT_TYPE": "GPE"}]
-date_pattern = [
-    {"IS_DIGIT": True, "LENGTH": 4},  # Year
-    {"TEXT": "-", "OP": "?"},
-    {"IS_DIGIT": True, "LENGTH": 2},   # Month
-    {"TEXT": "-", "OP": "?"},
-    {"IS_DIGIT": True, "LENGTH": 2},   # Day
-]
-matcher.add("ORIGIN", [origin_pattern])
-matcher.add("DESTINATION", [destination_pattern])
-matcher.add("DATE", [date_pattern])
+class NLP():
 
-def extract_itinerary(text):
+    def __init__(self) -> None:
+        
+        self.nlp = spacy.load("en_core_web_sm")
+        self.matcher = Matcher(self.nlp.vocab)
+        origin_pattern = [{"LOWER": {"IN": ["from", "leave"]}}, {"POS": "PROPN", "OP": "+", "ENT_TYPE": "GPE"}]
+        destination_pattern = [{"LOWER": {"IN": ["to", "tour", "visit"]}}, {"POS": "PROPN", "OP": "+", "ENT_TYPE": "GPE"}]
+        date_pattern = [
+            {"IS_DIGIT": True, "LENGTH": 4},  # Year
+            {"TEXT": "-", "OP": "?"},
+            {"IS_DIGIT": True, "LENGTH": 2},   # Month
+            {"TEXT": "-", "OP": "?"},
+            {"IS_DIGIT": True, "LENGTH": 2},   # Day
+        ]
+        self.matcher.add("ORIGIN", [origin_pattern])
+        self.matcher.add("DESTINATION", [destination_pattern])
+        self.matcher.add("DATE", [date_pattern])
 
-    doc = nlp(text)
-    matches = matcher(doc)
-    origin = None
-    destination = None
-    date = None
+    def validate_date(self, date_str):
+        try:
+            # Parse the string into a datetime object
+            date_object = datetime.strptime(date_str, '%Y-%m-%d')
+            
+            # Get today's date
+            today = datetime.now().date()
+            
+            # Check if the parsed date is not in the past
+            if date_object.date() >= today:
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
 
-    for match_id, start, end in matches:
-        if nlp.vocab.strings[match_id] == "ORIGIN":
-            origin = doc[start+1:end].text
-        elif nlp.vocab.strings[match_id] == "DESTINATION":
-            destination = doc[start+1:end].text
-        elif nlp.vocab.strings[match_id] == "DATE":
-            date = doc[start:end].text
-    return origin, destination, date
+    def extract_itinerary(self, text):
 
-def confirm_itinerary(origin, dest, date):
-    
-    while not origin:
-        print("What city are you leaving from?")
-        o = input()
-        origin_doc = nlp(o)
-        origin = [ent for ent in origin_doc.ents if ent.label_ == "GPE"][0]
+        doc = self.nlp(text)
+        matches = self.matcher(doc)
+        origin = None
+        destination = None
+        date = None
 
-    while not dest:
-        print("What city would you like to visit?")
-        d = input()
-        dest_doc = nlp(d)
-        dest = [ent for ent in dest_doc.ents if ent.label_ == "GPE"][0]
-    
-    # Maybe replace with date picker in webapp
-    while not date:
-        print("What date are you looking to fly? (Please enter in YYYY-MM-DD format)")
-        date_str = input()
-        date_doc = nlp(date_str)
-        for match_id, start, end in matcher(date_doc):
-            matched_span = date_doc[start:end]
-            date = matched_span.text
+        for match_id, start, end in matches:
+            if self.nlp.vocab.strings[match_id] == "ORIGIN":
+                origin = doc[start+1:end].text
+            elif self.nlp.vocab.strings[match_id] == "DESTINATION":
+                destination = doc[start+1:end].text
+            elif self.nlp.vocab.strings[match_id] == "DATE":
+                date = doc[start:end].text
+        return origin, destination, date
 
-    # # Maybe replace with dropdown in webapp
-    numTickets = None
-    while not numTickets:
-        print("How many tickets should I look for? (Must be greater than zero)")
-        tickets = int(input())
-        if tickets > 0: 
-            numTickets = tickets
+    def confirm_itinerary(self, origin, dest, date):
+        
+        while not origin:
+            print("What city are you leaving from?")
+            o = input()
+            origin_doc = self.nlp(o)
+            origin = [ent for ent in origin_doc.ents if ent.label_ == "GPE"][0]
 
-    print(f"Looking for {numTickets} ticket(s) from {origin} to {dest} on {date}")
-    return origin, dest, date, numTickets
+        while not dest:
+            print("What city would you like to visit?")
+            d = input()
+            dest_doc = self.nlp(d)
+            dest = [ent for ent in dest_doc.ents if ent.label_ == "GPE"][0]
+        
+        # Maybe replace with date picker in webapp
+        while not date:
+            print("What date are you looking to fly? (Please enter in YYYY-MM-DD format)")
+            date_str = input()
+            date_doc = self.nlp(date_str)
+            for match_id, start, end in self.matcher(date_doc):
+                matched_span = date_doc[start:end]
+                if self.validate_date(matched_span.text):
+                    date = matched_span.text
+                else:
+                    print("Invalid date")
 
-def chatbot():
-
-    print('Hello, how can I help you today?')
-    user_input = input()
-    origin, destination, date = extract_itinerary(user_input)
-    confirm_itinerary(origin, destination, date)
+        # Maybe replace with dropdown in webapp
+        numTickets = None
+        while not numTickets:
+            print("How many tickets should I look for? (Must be greater than zero)")
+            tickets = int(input())
+            if tickets > 0: 
+                numTickets = tickets
+        return origin, dest, date, numTickets 
 
 if __name__ == "__main__":
 
-    chatbot()
+    nlp = NLP()
+    print('Hello, how can I help you today?')
+    user_input = input()
+    origin, destination, date = nlp.extract_itinerary(user_input)
+    orig, dest, date, numTickets = nlp.confirm_itinerary(origin, destination, date)
+    print(f"Looking for {numTickets} ticket(s) from {orig} to {dest} on {date}")
